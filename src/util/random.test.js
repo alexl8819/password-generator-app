b8d1aacf96bfd68962048d63f6f5b1b5deb5797b
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-04-15 22:52:20 -0700

    fix: better testing name for random module

--- src/util/random.test.js
+++ src/util/random.test.js
@@ -6,7 +6,7 @@ import {
   SYMBOLS 
 } from './random';
 
-describe('Random', () => {
+describe('Random sequence generator', () => {
   test('Should generate a 13 character sequence based on a limit and custom alphabet', () => {
     expect(generateSequence(13, ALPHABET_LOWER + ALPHABET_UPPER + NUMBERS + SYMBOLS)).toHaveLength(13);
   });

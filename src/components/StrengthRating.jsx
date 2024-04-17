8084079b72d0639acffc798b3e61e8632518bbde
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-04-16 19:13:19 -0700

    remove unnecessary var and use array conversion function directly when rendering rating bars

--- src/components/StrengthRating.jsx
+++ src/components/StrengthRating.jsx
@@ -8,13 +8,12 @@ import styles from './StrengthRating.module.css';
 const cx = classnames.bind(styles);
 
 export default function StrengthRating ({ rating }) {
-  const ratingToArr = toArray(rating);
   return (
     <div className={styles.ratingBlock}>
       <p className={styles.ratingBlockStrengthtext}>strength</p>
       <ul className={styles.ratingBlockRatings} data-rating={rating.description}>
         {
-          ratingToArr.map((fill, index) => 
+          toArray(rating).map((fill, index) => 
             fill ? (<li key={index} className={cx({ 
               rating: true, ratingVeryweak: rating === Rating.VERY_WEAK, ratingWeak: rating === Rating.WEAK, ratingMedium: rating === Rating.MEDIUM, ratingStrong: rating === Rating.STRONG }
             )}></li>) : (<li key={index} className={styles.rating}></li>)

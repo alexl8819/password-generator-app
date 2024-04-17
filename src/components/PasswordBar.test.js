02545655ff6cd72ec977e375e2e2f19f82861c60
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-04-17 14:01:46 -0700

    refactor: add new test for PasswordBar component

--- src/components/PasswordBar.test.js
+++ src/components/PasswordBar.test.js
@@ -6,13 +6,16 @@ import PasswordBar from './PasswordBar';
 describe('PasswordBar component', () => {
   const passwordCopy = 'ptdtwVtFL1';
   const onCopy = jest.fn();
-  render(<PasswordBar passwordCopy={passwordCopy} onCopy={onCopy} />);
+
+  const { container } = render(<PasswordBar passwordCopy={passwordCopy} onCopy={onCopy} />);
+
+  const innerInput = container.querySelector('input[type="text"]');
 
   test('Should trigger onCopy fn when copy is called', async () => {
     fireEvent.click(screen.getByRole('button', { name: /copy/i }));
     await waitFor(() => expect(onCopy).toHaveBeenCalled());
   });
-  /*test('Should display passwordCopy prop as value', () => {
-    expect(screen.getByPlaceholderText('P4$5W0rD!')).toHaveValue(passwordCopy);
-  });*/
+  test('Should display passwordCopy prop as value', () => {
+    expect(innerInput).toHaveDisplayValue(passwordCopy);
+  });
 });
